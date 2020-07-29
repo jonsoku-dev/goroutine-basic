b29fe5ad06eecfcaf6af631f72ac21c31069bccc
ricky <ricky@rickyuicbookpro.lan>
2020-07-29 20:11:51 +0900

    defer 함수 및 naked return

--- src/github.com/learngo/main.go
+++ src/github.com/learngo/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+// return multiple value function
+func lenAndUpper(name string) (length int, uppercase string) {
+	// 함수가 끝나면 실행된다 "defer"
+	defer fmt.Println("I'm done!")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	// naked return !
+	// 사용자가 리턴값을 정해준대로 위에 변수가 존재하면 그대로 리턴한다.
+	return
 }
 
 func main() {
-	repeatMe("jongseok", "kazuko", "hiromi", "mother")
+	totalLength, _ := lenAndUpper("jongseok")
+	fmt.Println(totalLength)
 }
