b4804f628b50060372275aef9d485b26a20427a9
ricky <ricky@rickyuicbookpro.lan>
2020-07-30 00:37:44 +0900

    struct에 주입하여 리턴

--- src/github.com/learngo/main.go
+++ src/github.com/learngo/main.go
@@ -5,9 +5,19 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+// dto인가보다
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
 
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
@@ -23,6 +33,43 @@ func main() {
 func getPage(i int) {
 	baseURL := baseURL + "&start=" + strconv.Itoa(i*50)
 	fmt.Println("Requesting...", baseURL)
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	// Card 전체를 찾는다. Find는 복수로 가져오나보다. (*Selection)
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+
+	// job 정보를 추출한다.
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		extractJob(card)
+	})
+
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".title>a").Text())
+	location := cleanString(card.Find(".sjcl").Text())
+	salary := cleanString(card.Find("salaryText").Text())
+	summary := cleanString(card.Find(".summary").Text())
+	// struct 에 주입하여 리턴한다.
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary,
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
