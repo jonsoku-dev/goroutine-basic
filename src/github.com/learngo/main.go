fac6927e7b65668713907e86c6b5b10f3c5f5f9e
ricky <ricky@rickyuicbookpro.lan>
2020-07-29 20:15:41 +0900

    여러가지 for문

--- src/github.com/learngo/main.go
+++ src/github.com/learngo/main.go
@@ -16,7 +16,22 @@ func lenAndUpper(name string) (length int, uppercase string) {
 	return
 }
 
+func superAdd(numbers ...int) int {
+	// index가 처음
+	// for index, number := range numbers {
+	// 	fmt.Println(index, number)
+	// }
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
+	// }
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
 func main() {
-	totalLength, _ := lenAndUpper("jongseok")
-	fmt.Println(totalLength)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
