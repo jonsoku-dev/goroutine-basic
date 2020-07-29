b0ccedb30afc9a263acd7446730711197299fb96
ricky <ricky@rickyuicbookpro.lan>
2020-07-29 20:21:36 +0900

    switch

--- src/github.com/learngo/main.go
+++ src/github.com/learngo/main.go
@@ -39,6 +39,16 @@ func canIDrink(age int) bool {
 	}
 }
 
+func canIDrinkWithSwitch(age int) bool {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
 func main() {
-	fmt.Println(canIDrink(16))
+	fmt.Println(canIDrinkWithSwitch(16))
 }
